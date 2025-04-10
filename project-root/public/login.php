57106e37478d4b3547cacda3776b6263358b4579
wnzid <--replace-all>
2025-04-10 09:59:13 +0300

    v2.0.2

--- project-root/public/login.php
+++ project-root/public/login.php
@@ -1,16 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>User Login</title>
-</head>
-<body>
-    <h2>User Login</h2>
-    <form action="login_process.php" method="post">
-        Username: <input type="text" name="username" required><br>
-        Password: <input type="password" name="password" required><br>
-        <input type="submit" value="Login">
-    </form>
-    <p>New user? <a href="add_user.php">Register here</a>.</p>
-</body>
-</html>
+<?php
+$pageTitle = "User Login";
+include_once __DIR__ . '/../includes/header.php';
+?>
+<h2>User Login</h2>
+<?php if (isset($_GET['error'])): ?>
+    <p style="color:red;"><?php echo htmlspecialchars($_GET['error']); ?></p>
+<?php endif; ?>
+<form action="login_process.php" method="post">
+    <label>Username: <input type="text" name="username" required></label><br>
+    <label>Password: <input type="password" name="password" required></label><br>
+    <input type="submit" value="Login">
+</form>
+<p>New user? <a href="register.php">Register here</a>.</p>
+<?php
+include_once __DIR__ . '/../includes/footer.php';
+?>
